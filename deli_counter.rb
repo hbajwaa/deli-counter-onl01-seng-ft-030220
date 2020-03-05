0036eb40369d1fb18dce5e2fa20534e96f24bcbe
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 02:50:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,29 @@
 katz_deli = []
 
-def take_a_number(karz_deli, person)
-  katz_deli.push(person)
-  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
-end
-
-# ----------------------------------
-
 def line(katz_deli)
-  
-  if katz_deli.length == 0
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    line_number = 1
-    message = "The line is currently:"
-    katz_deli.each do |name|
-      message += " #{line_number}. #{name}"  
-      line_number += 1
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
     end
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
-  return katz_deli
 end
 
-# ----------------------------------
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
 
-def now_sering(katz_deli)
-  puts "currently serving #{katz_deli[0]}."
-  katz_deli.shift[0];
-end  
-  
-  
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
